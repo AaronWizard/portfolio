f7b016a51306e9a30dceb8ab05bf715f1c55593a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-07 18:43:57 -0400

    Site section components

---
+++ src/components/Activities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Activities extends React.Component {
+	render () {
+		return (
+			<h2>Activities</h2>
+		);
+	}
+}
+
+export default Activities;
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
-import resume from '../resume';
+//import resume from '../resume';
+
+import About from './About';
+import EmploymentHistory from './EmploymentHistory';
+import Education from './Education';
+import Activities from './Activities';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>
-				<p>{resume.profile}</p>
-				<p>{resume.contactInfo.street1}</p>
-				<p>{resume.contactInfo.city}</p>
-			</div>
+			<Container>
+				<h1>Aaron MacDonald Resume and Portfolio</h1>
+				<About />
+				<EmploymentHistory />
+				<Education />
+				<Activities />
+			</Container>
 		);
 	}
 }
