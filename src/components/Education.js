97c85505900bdccca3eacaec88496fd93694fd97
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-07 18:43:57 -0400

    Site section components

---
+++ src/components/Education.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Education extends React.Component {
+	render () {
+		return (
+			<h2>Education</h2>
+		);
+	}
+}
+
+export default Education;
---
+++ src/components/EmploymentHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class EmploymentHistory extends React.Component {
+	render () {
+		return (
+			<h2>Employment History</h2>
+		);
+	}
+}
+
+export default EmploymentHistory;
