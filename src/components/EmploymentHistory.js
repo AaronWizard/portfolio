7e1feac852d5c8fb63e4f08a538473690561b3e7
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-08 17:42:12 -0400

    Employer details

--- src/components/EmploymentHistory.js
+++ src/components/EmploymentHistory.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Row, Col, Accordion, Card } from 'react-bootstrap';
+import { Accordion } from 'react-bootstrap';
+
+import Employer from './Employer';
 
 import resume from '../resume';
 
@@ -7,23 +9,7 @@ const EmploymentHistory = () => {
 	return (
 		<Accordion>
 			{resume.employers.map((employer, index) =>
-				<Card key={index}>
-					<Accordion.Toggle as={Card.Header} eventKey={index}>
-						<Row>
-							<Col xs={5}>
-								<strong>{employer.company}</strong>
-							</Col>
-							<Col>{employer.jobTitle}</Col>
-							<Col xs={2}>{employer.location}</Col>
-							<Col>
-								{employer.fromDate} - {employer.toDate}
-							</Col>
-						</Row>
-					</Accordion.Toggle>
-					<Accordion.Collapse eventKey={index}>
-						<Card.Body>Hello</Card.Body>
-					</Accordion.Collapse>
-				</Card>
+				<Employer key={index} employer={employer} index={index} />
 			)}
 		</Accordion>
 	);
