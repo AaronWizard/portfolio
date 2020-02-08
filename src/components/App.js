11926e9e4d478d41aea7f44d2bd9f6edce4595e8
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-08 15:35:40 -0400

    NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
-//import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 //import resume from '../resume';
+
+import NavBar from './NavBar';
 
 import About from './About';
 import EmploymentHistory from './EmploymentHistory';
@@ -15,31 +17,19 @@ class App extends React.Component {
 	render() {
 		return (
 			<Router>
-				<h1>Aaron MacDonald&#39;s Resume and Portfolio</h1>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">About</Link>
-						</li>
-						<li>
-							<Link to="/employment">Employment</Link>
-						</li>
-						<li>
-							<Link to="/education">Education</Link>
-						</li>
-						<li>
-							<Link to="/activities">Activities</Link>
-						</li>
-					</ul>
-				</nav>
-				<Switch>
-					<Route exact path="/" component={About} />
-					<Route exact path="/employment"
-						component={EmploymentHistory} />
-					<Route exact path="/education" component={Education} />
-					<Route exact path="/activities" component={Activities} />
-					<Route component={PageNotFound} />
-				</Switch>
+				<Container>
+					<h1>Aaron MacDonald&#39;s Resume and Portfolio</h1>
+					<NavBar />
+					<Switch>
+						<Route exact path="/" component={About} />
+						<Route exact path="/employment"
+							component={EmploymentHistory} />
+						<Route exact path="/education" component={Education} />
+						<Route exact path="/activities"
+							component={Activities} />
+						<Route component={PageNotFound} />
+					</Switch>
+				</Container>
 			</Router>
 		);
 	}
