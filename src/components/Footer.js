2e2f06f3ad397ab553b8ee148307c4a8f14e5b20
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-08 23:54:54 -0400

    Footer background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 import ThirdPartyLink from './ThirdPartyLink';
 
@@ -6,20 +7,19 @@ import resume from '../resume';
 
 const YEAR = 2020;
 
-const style = {
-	position: 'fixed',
-	bottom: '0'
-};
-
 const Footer = () => {
 	return (
-		<footer style={style}>
-			&#169; {YEAR} Aaron MacDonald
-			&nbsp;|&nbsp;
-			<ThirdPartyLink url={resume.contactInfo.linkedInURL}
-				text="LinkedIn" />
-			&nbsp;|&nbsp;
-			<ThirdPartyLink url={resume.github} text="Github" />
+		<footer className="fixed-bottom">
+			<Card>
+				<Card.Body>
+					&#169; {YEAR} Aaron MacDonald
+					&nbsp;|&nbsp;
+					<ThirdPartyLink url={resume.contactInfo.linkedInURL}
+						text="LinkedIn" />
+					&nbsp;|&nbsp;
+					<ThirdPartyLink url={resume.github} text="Github" />
+				</Card.Body>
+			</Card>
 		</footer>
 	);
 };
