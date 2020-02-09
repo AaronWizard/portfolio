e47b3f2fc32dc70272c6f9600a625bc8c445c16e
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-09 11:01:32 -0400

    Fixed overlapping footer. Added padding above title.

--- src/components/App.js
+++ src/components/App.js
@@ -13,21 +13,28 @@ import PageNotFound from './PageNotFound';
 
 import Footer from './Footer';
 
+const style = {
+	paddingTop: '0.5em',
+	paddingBottom: '90px'
+};
+
 const App = () => {
 	return (
 		<Router>
 			<Container>
-				<h1>Aaron MacDonald&#39;s Resume and Portfolio</h1>
-				<NavBar />
-				<Switch>
-					<Route exact path="/" component={About} />
-					<Route exact path="/employment"
-						component={EmploymentHistory} />
-					<Route exact path="/education" component={Education} />
-					<Route exact path="/activities"
-						component={Activities} />
-					<Route component={PageNotFound} />
-				</Switch>
+				<div style={style}>
+					<h1>Aaron MacDonald&#39;s Resume and Portfolio</h1>
+					<NavBar />
+					<Switch>
+						<Route exact path="/" component={About} />
+						<Route exact path="/employment"
+							component={EmploymentHistory} />
+						<Route exact path="/education" component={Education} />
+						<Route exact path="/activities"
+							component={Activities} />
+						<Route component={PageNotFound} />
+					</Switch>
+				</div>
 				<Footer />
 			</Container>
 		</Router>
